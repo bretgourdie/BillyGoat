4d694775feb1edecb7f5ea5fc1dda98bc400ff06
Bret Gourdie <bgourdie@THETECHS.SDI.LOCAL>
2016-07-13 10:01:20 -0400

    Removed throws from main
    No longer throws IOException

--- src/billyGoat/BillifyMain.java
+++ src/billyGoat/BillifyMain.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class BillifyMain {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		Billify bill = new Billify();
 		
 		int[][] inputs = initializeInputs();
